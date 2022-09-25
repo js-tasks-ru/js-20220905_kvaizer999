9e269aed007a4eada1f6a847b82fda051ea4b9c5
Kvaizer <kvaizer999@mail.ru>
2022-09-25 17:16:54 +0300

    '5task'

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,161 @@
 export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+  element;
+  subElements = {};
 
+  constructor(headerConfig = [], {data = [], sorted = {}} = {}) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.id = sorted.id;
+    this.order = sorted.order;
+    this.render();
+    this.sort(this.id, this.order);
+    this.addEventListeners();
+  }
+
+  sortData(field, order) {
+    const arr = [...this.data];
+    const column = this.headerConfig.find(item => item.id === field);
+    const {sortType} = column;
+    const directions = {asc: 1, desc: -1};
+    const direction = directions[order];
+
+    return arr.sort((a, b) => {
+      switch (sortType) {
+      case 'number':
+        return direction * (a[field] - b[field]);
+
+      case 'string':
+        return direction * a[field].localeCompare(b[field], ['ru', 'en']);
+
+      default:
+        return direction * (a[field] - b[field]);
+      }
+    });
+  }
+
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+    const allColumns = this.element.querySelectorAll('.sortable-table__cell[data-id]');
+    const currentColumn = this.element.querySelector(`.sortable-table__cell[data-id=${field}]`);
+
+
+    allColumns.forEach(column => {
+      column.dataset.order = '';
+    });
+
+    currentColumn.dataset.order = order;
+
+    this.subElements.body.innerHTML = this.getTableRows(sortedData);
+  }
+
+  getTableHeader() {
+    return `
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.headerConfig.map(item => this.getHeaderCell(item)).join('')}
+        </div>
+`;
+  }
+
+  getHeaderCell({id, title, sortable}) {
+    return `
+        <div data-id=${id} data-sortable=${sortable} class="sortable-table__cell" >
+            <span>${title}</span>
+            <span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+            </span>
+        </div>
+    `;
+  }
+
+  getTableBody() {
+    return `
+        <div data-element="body" class="sortable-table__body">
+            ${this.getTableRows(this.data)}
+        </div>
+    `;
+  }
+
+  getTableRows(data = []) {
+    return data.map(item => {
+      return `
+        <a href="/products/${item.id}" class="sortable-table__row">
+            ${this.getTableRow(item)}
+        </a>
+      `;
+    }).join('');
+  }
+
+  getTableRow(item) {
+    const cells = this.headerConfig.map(({id, template}) => {
+      return {id, template};
+    });
+
+    return cells.map(({id, template}) => {
+      return template ? template(item[id]) : `<div class="sortable-table__cell">${item[id]}</div>`;
+    }).join('');
+  }
+
+  getTable() {
+    return `
+       <div class="sortable-table">
+           ${this.getTableHeader()}
+           ${this.getTableBody()}
+       </div>
+    `;
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getTable();
+    const element = wrapper.firstElementChild;
+    this.element = element;
+    this.subElements = this.getSubElements(element);
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  onHeaderClickHandler = (event) => {
+    let elem = event.target.parentNode;
+
+    while (!elem.dataset.sortable) {
+      elem = elem.parentNode;
+    }
+
+    if (elem.dataset.sortable === 'false') {
+      return;
+    }
+
+    this.order = this.order === 'asc' ? 'desc' : 'asc';
+    this.sort(elem.dataset.id, this.order);
+  }
+
+  addEventListeners() {
+    const header = this.element.querySelector('.sortable-table__header');
+    header.addEventListener('onclick', this.onHeaderClickHandler);
+  }
+
+  removeEventListeners() {
+    const header = this.element.querySelector('.sortable-table__header');
+    header.removeEventListener('onclick', this.onHeaderClickHandler);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.removeEventListeners();
   }
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,85 @@
 class Tooltip {
-  initialize () {
+  constructor() {
+    this.render();
+    this.addEventListeners();
+  }
 
+  static stringParser() {
+    const divs = document.querySelectorAll('[data-tooltip]');
+    [...divs].forEach(div => {
+      const children = div.childNodes;
+      [...children].forEach(item => {
+        const spans = [];
+        if (item.nodeType === Node.TEXT_NODE) {
+          item.data.split(' ').forEach(word => {
+            if (word.trim() !== '') {
+              const span = document.createElement('span');
+              span.innerHTML = `${word} `;
+              spans.push(span);
+            }
+          });
+          item.replaceWith(...spans);
+        }
+      });
+    });
+  }
+
+  initialize() {
+    Tooltip.stringParser();
+  }
+
+  template() {
+    return `
+       <div class="tooltip" hidden></div>
+    `;
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template();
+    this.element = wrapper.firstElementChild;
+    const container = document.querySelector('[data-tooltip]');
+    container.append(this.element);
+  }
+
+  onMouseOverHandler = (event) => {
+    if (event.target.tagName === 'SPAN') {
+      this.element.hidden = false;
+      this.element.innerHTML = event.target.innerText;
+      this.element.style.left = (event.clientX + 10) + 'px';
+      this.element.style.top = (event.clientY + 10) + 'px';
+    } else {
+      this.element.hidden = true;
+    }
+  };
+
+  onMouseLeaveHandler = () => {
+    this.element.hidden = true;
+    this.element.innerHTML = '';
+  }
+
+  addEventListeners() {
+    const container = document.querySelector('[data-tooltip]');
+    container.addEventListener('mouseover', this.onMouseOverHandler);
+    container.addEventListener('mouseleave', this.onMouseLeaveHandler);
+  }
+
+  removeEventListeners() {
+    const container = document.querySelector('[data-tooltip]');
+    container.removeEventListener('mouseover', this.onMouseOverHandler);
+    container.removeEventListener('mouseleave', this.onMouseLeaveHandler);
+  }
+
+  remove() {
+    this.element.remove();
+    this.removeEventListeners();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
 export default Tooltip;
+
